d188382638cc932fb25104e60a0bd8cc154be187
geoffont <geoffroy.font@gmail.com>
2022-10-27 13:51:44 +0200

    camelCase finish

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 /*--------------fonction slide-----------*/
 
-let img__slider = document.getElementsByClassName("img__slider");
+let imgSlider = document.getElementsByClassName("img__slider");
 
 let etape = 0;
 
-let nbr__img = img__slider.length;
+let nbrImg = imgSlider.length;
 
 let precedent = document.querySelector(".precedent");
 let suivant = document.querySelector(".suivant");
 
 function enleverActiveImages() {
-  for (let i = 0; i < nbr__img; i++) {
-    img__slider[i].classList.remove("active");
+  for (let i = 0; i < nbrImg; i++) {
+    imgSlider[i].classList.remove("active");
   }
 }
 
 suivant.addEventListener("click", function () {
   etape++;
-  if (etape >= nbr__img) {
+  if (etape >= nbrImg) {
     etape = 0;
   }
   enleverActiveImages();
-  img__slider[etape].classList.add("active");
+  imgSlider[etape].classList.add("active");
 });
 
 precedent.addEventListener("click", function () {
   etape--;
   if (etape < 0) {
-    etape = nbr__img - 1;
+    etape = nbrImg - 1;
   }
   enleverActiveImages();
-  img__slider[etape].classList.add("active");
+  imgSlider[etape].classList.add("active");
 });
 /*------------------fonction formulaire------*/
 
@@ -47,13 +47,15 @@ function closePopup() {
 document
   .getElementById("formulaire")
   .addEventListener("submit", function (event) {
-    let inpuText = document.querySelector(".inpuText").value;
     
+    let inpuText = document.querySelector(".inpuText").value;
+   
     event.preventDefault();
-
+    
     let inputSubmit = document.querySelector(".inputSubmit");
+    
     inputSubmit.innerHTML = inpuText;
-
+    
 
     if (getComputedStyle(systeme_solaire).display != "none") {
       systeme_solaire.style.display = "none";
@@ -63,8 +65,9 @@ document
     } else {
       systeme_solaire.style.display = "block";
     }
+   
   });
-
+  
 /*-----------------fonction scroll---------------*/
 window.addEventListener("scroll", function () {
   const distance = window.scrollY;
